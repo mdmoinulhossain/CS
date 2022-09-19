adcd814db169285d898c9423fe7c64f3de7cc1f9
MD. MOINUL HOSSAIN <57934796+mdmoinulhossain@users.noreply.github.com>
2022-09-19 16:37:24 +0600

    AWS

---
+++ cloud_computing/AWS-from-A-to-Z/footer.js
@@ -0,0 +1,46 @@
+document.write('\
+	\
+	<!DOCTYPE HTML>\
+	<html>\
+	<head>\
+	<link rel="stylesheet" href="assets/css/main.css" />\
+	<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>\
+	<script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>\
+	</head>\
+	<body class="is-preload">\
+	</div>\
+	<!-- Wrapper -->\
+	<div id="wrapper">\
+	<!-- Footer -->\
+	<footer id="footer">\
+	<div class="inner">\
+	<section>\
+	<h2>Get in touch for any clarifications or suggestions!</h2>\
+	<sub>Beware of disabled links. They will be enabled as soon as I create the respective sketchnotes.</sub>\
+	<ul class="icons">\
+	<li><a href="https://www.linkedin.com/in/dasika-madhu-nimeshika/" target="_blank" class="label"><span class="iconify" data-icon="logos:linkedin-icon" data-inline="false"></span></a></li>\
+	<li><a href="https://twitter.com/xxMadBluexx" target="_blank" class="label"><span class="iconify" data-icon="logos:twitter" data-inline="false"></span></a></li>\
+	<li><a href="https://github.com/DasikaMadhu" target="_blank" class="label"><span class="iconify" data-icon="icon-park:github-one" data-inline="false"></span></a></li>\
+	<li><a href="mailto:dasikamadhu@gmail.com" target="_blank" class="label"><span class="iconify" data-icon="icon-park:send-email" data-inline="false"></span></a></li>\
+	<li><a href="https://madhue.medium.com" target="_blank" class="label"><span class="iconify" data-icon="cib:medium-m" data-inline="false"></span></a></li>\
+	<li><a href="https://madhue-portfolio.com" target="_blank" class="label"><span class="iconify" data-icon="gg:website" data-inline="false"></span></a></li>\
+	</ul>\
+	<a href="https://www.buymeacoffee.com/nimeshika" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;" ></a>\
+	\
+	</section>\
+	<ul class="copyright">\
+	<li>Content &copy; Dasika Madhu Nimeshika. All rights reserved</li>\
+	</ul>\
+	</div>\
+	</footer>\
+	</div>\
+	<!-- Scripts -->\
+	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>\
+	<script src="assets/js/jquery.min.js"></script>\
+	<script src="assets/js/browser.min.js"></script>\
+	<script src="assets/js/breakpoints.min.js"></script>\
+	<script src="assets/js/util.js"></script>\
+	<script src="assets/js/main.js"></script>\
+	</body>\
+	</html>\
+	');
